45728916d4557e990581f6e588aa635f8410b210
md_naciri <med.ar.naciri@gmail.com>
2023-03-11 23:37:06 +0100

    Seeding permissions and roles

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
-class RolesRolesAndPermissionsSeeder extends Seeder
+class RolesAndPermissionsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
